901c0547bad66efad7a05492a8a80385cd571e9f
Iván SZKIBA <szkiba@users.noreply.github.com>
2025-08-21 17:53:22 +0000

    test for async callback

---
+++ mqtt/testdata/connect_async_test.cjs
@@ -0,0 +1,34 @@
+const mqtt = require("k6/x/mqtt")
+const assert = require("k6/x/assert")
+
+var connectHandlerCalled = false
+var endHandlerCalled = false
+
+module.exports = () => {
+  const client = new mqtt.Client()
+
+  client.on("connect", async () => {
+    assert.true(client.connected, "Client should be connected after connect event")
+
+    connectHandlerCalled = true
+
+    await client.endAsync()
+  })
+
+  client.on("end", () => {
+    assert.false(client.connected, "Client should not be connected after end event")
+
+    endHandlerCalled = true
+  })
+
+  assert.false(client.connected, "Client should not be connected initially")
+
+  client.connect(__ENV.MQTT_BROKER_ADDRESS)
+
+  assert.true(client.connected, "Client should be connected after connect call")
+}
+
+module.exports.teardown = () => {
+  assert.true(connectHandlerCalled, "Connect handler was not called")
+  assert.true(endHandlerCalled, "End handler was not called")
+}
